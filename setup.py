c82ebcb7a87bb038499c88322a838d9b0715c1a3
Manojkumar8899 <146886392+Manojkumar8899@users.noreply.github.com>
2023-11-21 20:11:50 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='mypackage',
     version='0.1',
-    packages=find_packages(),
-    install_requires=[
-        
-    ],
+    author= 'manoj',
     
+    packages=find_packages(),
+    install_requires=[ 
+        "requests>=2.25.1",
+        "pandas>=1.2.0"
+        "beautifulsoup4>=4.9.3"
+    ],
+    python_requires="3.6"
 )
