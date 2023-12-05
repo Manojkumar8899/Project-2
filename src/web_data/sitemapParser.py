9b90843ddd3c5af98838ce3d6fd84120f9c1f508
Manojkumar8899 <146886392+Manojkumar8899@users.noreply.github.com>
2023-12-05 16:07:03 -0500

    Update sitemapParser.py

--- src/web_data/sitemapParser.py
+++ src/web_data/sitemapParser.py
@@ -5,15 +5,57 @@ import os
 
 
 class XMLSitemapExtractor:
-    """Extract URLs from XML sitemaps of a given website."""
+   """Extract URLs from XML sitemaps of a given website.
+
+    Args:
+        website_url (str): The URL of the website.
+
+    Attributes:
+        website_url (str): The URL of the website.
+        extracted_dataframes (dict): A dictionary to store extracted DataFrames.
+
+    Methods:
+        retrieve_content(url):
+            Retrieve content from the specified URL.
+
+        process_sitemap(sitemap_url):
+            Process an individual sitemap URL.
+
+        process_all_sitemaps():
+            Retrieve and process all sitemaps found in the website's robots.txt.
+
+        create_dataframe(url_list):
+            Convert a list of URLs into a DataFrame.
+
+        split_url_parts(dataframe):
+            Split URLs into components and add them as separate columns.
+
+        save_to_csv(directory="extracted_sitemaps"):
+            Save the extracted data to CSV files.
+    """
+
 
     def __init__(self, website_url):
+
+         """Initialize the XMLSitemapExtractor.
+
+        Args:
+            website_url (str): The URL of the website.
+        """
+        
         self.website_url = website_url
         self.extracted_dataframes = {}
         self.process_all_sitemaps()
 
     def retrieve_content(self, url):
-        """Retrieve content from the specified URL."""
+        """Retrieve content from the specified URL.
+
+        Args:
+            url (str): The URL to retrieve content from.
+
+        Returns:
+            bytes or None: The content of the URL if successful, otherwise None.
+        """
         try:
             response = requests.get(url)
             response.raise_for_status()
@@ -23,7 +65,11 @@ class XMLSitemapExtractor:
             return None
 
     def process_sitemap(self, sitemap_url):
-        """Process an individual sitemap URL."""
+       """Process an individual sitemap URL.
+
+        Args:
+            sitemap_url (str): The URL of the sitemap.
+        """
         content = self.retrieve_content(sitemap_url)
         if content:
             soup = BeautifulSoup(content, "xml")
@@ -40,12 +86,26 @@ class XMLSitemapExtractor:
                     self.process_sitemap(line.split(": ")[1].strip())
 
     def create_dataframe(self, url_list):
-        """Convert a list of URLs into a DataFrame."""
+        """Convert a list of URLs into a DataFrame.
+
+        Args:
+            url_list (list): List of URLs.
+
+        Returns:
+            pd.DataFrame: A DataFrame containing the URLs.
+        """
         df = pd.DataFrame(url_list, columns=["ExtractedURLs"])
         return self.split_url_parts(df)
 
     def split_url_parts(self, dataframe):
-        """Split URLs into components and add them as separate columns."""
+        """Split URLs into components and add them as separate columns.
+
+        Args:
+            dataframe (pd.DataFrame): Input DataFrame with URLs.
+
+        Returns:
+            pd.DataFrame: Updated DataFrame with additional columns.
+        """
         max_depth = 0
         for i, url in enumerate(dataframe["ExtractedURLs"]):
             parts = url.replace(self.website_url, "").strip("/").split("/")
@@ -58,7 +118,11 @@ class XMLSitemapExtractor:
         return dataframe
 
     def save_to_csv(self, directory="extracted_sitemaps"):
-        """Save the extracted data to CSV files."""
+       """Save the extracted data to CSV files.
+
+        Args:
+            directory (str, optional): The directory to save CSV files. Defaults to "extracted_sitemaps".
+        """
         if not os.path.exists(directory):
             os.makedirs(directory)
         for key, df in self.extracted_dataframes.items():
